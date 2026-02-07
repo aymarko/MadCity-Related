41a7b4b1c06840046b936221d377d4240edc41dc
aymarko <140316397+aymarko@users.noreply.github.com>
2026-02-07 23:23:07 +0100

    Update MCY Modlist.lua

--- MCY Modlist.lua
+++ MCY Modlist.lua
@@ -4,7 +4,6 @@ local ModList = {
 
     [6395741] = "Exploit Hunter", -- COOL12JACK
     [855327900] = "Exploit Hunter", -- implexxa
-    [9920991471] = "Test",
 
     -- Devs
     [116481933] = "Friends", -- Rootie_DaHoodie
